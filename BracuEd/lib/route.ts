4b2d19822c72317549666c41fa8ba3673b38d9f3
Monowar Zamil Abir <abirofficial254@gmail.com>
2025-05-03 10:24:45 +0600

    Delete BracuEd directory

--- BracuEd/lib/route.ts
+++
@@ -1,7 +0,0 @@
-export const publicRoutes = ["/"];
-export const authRoutes = ["/login", "/register"];
-export const protectedRoute = ["/dashboard"];
-
-export const apiRoutePrefix = "/api/auth";
-export const DEFAULT_LOGIN_REDIRECT = "/";
-export const LOGIN_REDIRECT = "/login";
